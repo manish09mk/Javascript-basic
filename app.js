75ad2982ecd9a638a811b78d87a4d89df4007ddf
manish09mk <mk04manishkashyap@gmail.com>
2025-05-13 13:49:13 +0530

    First commmit

---
+++ app.js
@@ -0,0 +1,29 @@
+//operator
+//let a = 10;
+//let b = 5;
+//b = a; // a me b ki value assign ho jyega
+//console.log(b);
+
+
+
+
+// comparison operator
+//let age = 18;
+//console.log(age>18);// false
+
+//5==5;
+// non number comparisiom
+//'a' > 'A' // true ..because compare unicode of there latter
+let color = "red";
+if(color == "red"){
+
+    console.log("stop")
+}
+if(color == "yellow"){
+
+    console.log("wait")
+}
+if(color == "green"){
+
+    console.log("run")
+}
